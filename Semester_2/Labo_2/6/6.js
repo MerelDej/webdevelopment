78d1daf9e0204daa65e47b75ea3bfa48ca504612
MerelDej <merel.dejaegere@student.vives.be>
2024-02-23 21:22:52 +0100

    Labo 2

--- Semester_2/Labo_2/6/6.js
+++ Semester_2/Labo_2/6/6.js
@@ -1,8 +1,9 @@
 let btnKopieer = document.getElementById("btnKopieer");
-btnKopieer.addEventListener("click", kopieer);
 
 const kopieer = () => {
     let txtInput = document.getElementById("txtInput");
     let tekst = txtInput.value;
     console.log(tekst);
 }
+
+btnKopieer.addEventListener("click", kopieer)
