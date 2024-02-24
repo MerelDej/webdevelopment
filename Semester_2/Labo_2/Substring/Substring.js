f9ac85f9f4af62dea60cee76eba7de84982ab631
MerelDej <merel.dejaegere@student.vives.be>
2024-02-24 10:07:54 +0100

    Labo 2

--- Semester_2/Labo_2/Substring/Substring.js
+++ Semester_2/Labo_2/Substring/Substring.js
@@ -9,8 +9,8 @@ const substring = () => {
     let nrEen= document.getElementById("nummerEen");
     let nrTwee= document.getElementById("nummerTwee");
 
-    let g1 = parseInt(nrEen, 10)
-    let g2 = parseInt(nrTwee, 10)
+    let g1 = parseInt(nrEen.value, 10)
+    let g2 = parseInt(nrTwee.value, 10)
     txtOutput.innerHTML = txtInput.value.substring(g1, g2);
 }
 window.addEventListener('load',setup);
