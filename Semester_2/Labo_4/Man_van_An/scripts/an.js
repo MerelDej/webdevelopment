8773f2699418b44418cac330a1234d3afc9a98e7
MerelDej <merel.dejaegere@student.vives.be>
2024-03-07 17:52:03 +0100

    Labo 4

--- Semester_2/Labo_4/Man_van_An/scripts/an.js
+++ Semester_2/Labo_4/Man_van_An/scripts/an.js
@@ -1,5 +1,10 @@
 const setup = () => {
-	// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
+	let text = "De man van An geeft geen hand aan ambetante verwanten";
+    for(let i = 0; i < text.length; ){
+        let aantal= 0;
+        let index= text.indexOf("an", i);
+        aantal += index;
+        console.log(aantal);
+    }
 }
-
 window.addEventListener("load", setup);
--- Semester_2/Labo_4/Spaties_met_functies/scripts/spaties.js
+++ Semester_2/Labo_4/Spaties_met_functies/scripts/spaties.js
@@ -1,5 +1,18 @@
 const setup = () => {
-	// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
+    let btn=document.getElementById("btn");
+    btn.addEventListener("click", button);
 }
 
+const button = () => {
+    let tekst = document.getElementById("text");
+    let resultaat = "";
+    for(let i = 0; i < tekst.value.length; i++){
+        if(tekst.value.charAt(i) === " "){
+            resultaat += tekst.value.charAt(i);
+        } else{
+            resultaat += tekst.value.charAt(i) + " ";
+        }
+    }
+    console.log(resultaat.trim());
+}
 window.addEventListener("load", setup);
