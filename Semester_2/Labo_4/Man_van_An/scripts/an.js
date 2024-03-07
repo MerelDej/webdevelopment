be016e941554b8d568d393d750ac397eb9d6c590
MerelDej <merel.dejaegere@student.vives.be>
2024-03-07 18:51:02 +0100

    Labo 4

--- Semester_2/Labo_4/Man_van_An/scripts/an.js
+++ Semester_2/Labo_4/Man_van_An/scripts/an.js
@@ -1,9 +1,9 @@
 const setup = () => {
-    let text = "De man van An geeft geen hand aan ambetante verwanten";
+    let text = "De man van An geeft geen hand aan ambetante verwanten".toLowerCase();
     let aantal= 0;
     for(let i = 0; i < text.length; i++){
         let ind = text.indexOf("an", i);
-        if(ind !== text.indexOf("an", i++)){
+        if(ind !== text.indexOf("an", i+1)){
             aantal++;
         }
     }
