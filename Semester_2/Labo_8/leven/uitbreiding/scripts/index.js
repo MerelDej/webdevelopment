35fa208140fcf8af9c76400306adac5ffa5b64ec
MerelDej <merel.dejaegere@student.vives.be>
2024-04-21 10:55:23 +0200

    labo 8

--- Semester_2/Labo_8/leven/uitbreiding/scripts/index.js
+++ Semester_2/Labo_8/leven/uitbreiding/scripts/index.js
@@ -7,7 +7,7 @@ const bereken = (i, g) => {
     let dagen = document.querySelector("#dag");
     let aantal = (i - g)/86400000;
     let l = i.getDate() + "/" + (i.getMonth()+1) + "/" + i.getFullYear();
-    return dagen.innerHTML = "Er zijn al " + Math.floor(aantal) + " dagen gepasseert sinds " + l + ".";;
+    return dagen.innerHTML = "Er zijn al " + Math.floor(aantal) + " dagen gepasseert.";
 }
 
 const dat = () => {
